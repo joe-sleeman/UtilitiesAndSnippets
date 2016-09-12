333052b4466eaf09a8bd2a4de2ac3fdec2a88a46
Joe Sleeman <sleeman.joseph@gmail.com>
2016-09-12 17:13:21 +1200

    Initial commit. Committing Select Box Swapper

---
+++ SelectBoxSwapper/SelectBoxSwapper/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SelectBoxSwapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clbItems = new System.Windows.Forms.CheckedListBox();
+            this.clbAddedItems = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddNewItem = new System.Windows.Forms.Button();
+            this.txtNewItem = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnPurge = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clbItems
+            // 
+            this.clbItems.CheckOnClick = true;
+            this.clbItems.FormattingEnabled = true;
+            this.clbItems.Location = new System.Drawing.Point(6, 12);
+            this.clbItems.Name = "clbItems";
+            this.clbItems.Size = new System.Drawing.Size(237, 319);
+            this.clbItems.TabIndex = 0;
+            // 
+            // clbAddedItems
+            // 
+            this.clbAddedItems.CheckOnClick = true;
+            this.clbAddedItems.FormattingEnabled = true;
+            this.clbAddedItems.Location = new System.Drawing.Point(330, 12);
+            this.clbAddedItems.Name = "clbAddedItems";
+            this.clbAddedItems.Size = new System.Drawing.Size(237, 319);
+            this.clbAddedItems.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnPurge);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnAddNewItem);
+            this.groupBox1.Controls.Add(this.txtNewItem);
+            this.groupBox1.Controls.Add(this.btnRemove);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.clbAddedItems);
+            this.groupBox1.Controls.Add(this.clbItems);
+            this.groupBox1.Location = new System.Drawing.Point(11, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(580, 383);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 346);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Create new item:";
+            // 
+            // btnAddNewItem
+            // 
+            this.btnAddNewItem.Location = new System.Drawing.Point(434, 345);
+            this.btnAddNewItem.Name = "btnAddNewItem";
+            this.btnAddNewItem.Size = new System.Drawing.Size(133, 23);
+            this.btnAddNewItem.TabIndex = 5;
+            this.btnAddNewItem.Text = "Add New Item";
+            this.btnAddNewItem.UseVisualStyleBackColor = true;
+            this.btnAddNewItem.Click += new System.EventHandler(this.btnAddNewItem_Click);
+            // 
+            // txtNewItem
+            // 
+            this.txtNewItem.Location = new System.Drawing.Point(131, 347);
+            this.txtNewItem.Name = "txtNewItem";
+            this.txtNewItem.Size = new System.Drawing.Size(296, 20);
+            this.txtNewItem.TabIndex = 4;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(249, 41);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "<<< ";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(249, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = ">>>";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnPurge
+            // 
+            this.btnPurge.Location = new System.Drawing.Point(249, 308);
+            this.btnPurge.Name = "btnPurge";
+            this.btnPurge.Size = new System.Drawing.Size(75, 23);
+            this.btnPurge.TabIndex = 7;
+            this.btnPurge.Text = "Purge";
+            this.btnPurge.UseVisualStyleBackColor = true;
+            this.btnPurge.Click += new System.EventHandler(this.btnPurge_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 392);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Select Box Swapper";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clbItems;
+        private System.Windows.Forms.CheckedListBox clbAddedItems;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddNewItem;
+        private System.Windows.Forms.TextBox txtNewItem;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnPurge;
+    }
+}
+
---
+++ SelectBoxSwapper/SelectBoxSwapper/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SelectBoxSwapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            generateSeedItems();
+        }
+
+        /// <summary>
+        /// Will add an item to added items list.
+        /// </summary>
+        private void addItems()
+        {
+            // Find our checked items, and add them to added items list.
+            foreach (int i in clbItems.CheckedIndices)
+                clbAddedItems.Items.Add(clbItems.Items[i]);
+
+            // Now lets remove them from the original item list.
+            foreach (var item in clbItems.CheckedItems.OfType<string>().ToList())
+                clbItems.Items.Remove(item);
+        }
+
+        /// <summary>
+        /// Will remove an item from added items list.
+        /// </summary>
+        private void removeItems()
+        {
+            foreach (int i in clbAddedItems.CheckedIndices)
+                clbItems.Items.Add(clbAddedItems.Items[i]);
+
+            foreach (var item in clbAddedItems.CheckedItems.OfType<string>().ToList())
+                clbAddedItems.Items.Remove(item);
+        }
+
+        private void removeAllItems()
+        {
+            foreach (var item in clbAddedItems.CheckedItems.OfType<string>().ToList())
+                clbAddedItems.Items.Remove(item);
+            foreach (var item in clbItems.CheckedItems.OfType<string>().ToList())
+                clbItems.Items.Remove(item);
+        }
+
+        /// <summary>
+        /// Generate a bunch of seed items for when the application starts.
+        /// </summary>
+        private void generateSeedItems()
+        {
+            clbItems.Items.Add("When hungry, eat your rice.");
+            clbItems.Items.Add("When tired, close your eyes.");
+            clbItems.Items.Add("Fools may laugh at me");
+            clbItems.Items.Add("but wise men");
+            clbItems.Items.Add("will know");
+            clbItems.Items.Add("what I mean.");
+            clbItems.Items.Add("-Lin-Chi");
+        }
+
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            addItems();
+        }
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            removeItems();
+        }
+
+        private void btnAddNewItem_Click(object sender, EventArgs e)
+        {
+            string item = txtNewItem.Text;
+            if (item.Length > 0)
+                clbItems.Items.Add(item);
+            else
+                MessageBox.Show("Please enter a string in to the provided text box.", 
+                    "No String Error");
+        }
+
+        private void btnPurge_Click(object sender, EventArgs e)
+        {
+            removeAllItems();
+        }
+
+    }
+}
