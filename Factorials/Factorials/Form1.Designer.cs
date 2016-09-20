ef0b8808609db003c8769d3786a15948879aba41
Joe Sleeman <sleeman.joseph@gmail.com>
2016-09-20 13:16:55 +1200

    Recursive & iterative factorials

---
+++ Factorials/Factorials/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Factorials
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIterative = new System.Windows.Forms.Button();
+            this.btnRecursive = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnIterative
+            // 
+            this.btnIterative.Location = new System.Drawing.Point(6, 60);
+            this.btnIterative.Name = "btnIterative";
+            this.btnIterative.Size = new System.Drawing.Size(75, 23);
+            this.btnIterative.TabIndex = 0;
+            this.btnIterative.Text = "Iterative";
+            this.btnIterative.UseVisualStyleBackColor = true;
+            this.btnIterative.Click += new System.EventHandler(this.btnIterative_Click);
+            // 
+            // btnRecursive
+            // 
+            this.btnRecursive.Location = new System.Drawing.Point(95, 60);
+            this.btnRecursive.Name = "btnRecursive";
+            this.btnRecursive.Size = new System.Drawing.Size(75, 23);
+            this.btnRecursive.TabIndex = 1;
+            this.btnRecursive.Text = "Recursive";
+            this.btnRecursive.UseVisualStyleBackColor = true;
+            this.btnRecursive.Click += new System.EventHandler(this.btnRecursive_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "lblResult";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnRecursive);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnIterative);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 96);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(203, 121);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Factorials";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIterative;
+        private System.Windows.Forms.Button btnRecursive;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Factorials/Factorials/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorials
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Recursive version of factorial.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private int recursiveFactorial(int n)
+        {
+            // If we are 1, simply return 1.
+            if (n <= 1)
+                return 1;
+            // Otherwise, we want to call ourself, decrementing n.
+            return n * recursiveFactorial(n - 1);
+        }
+
+        /// <summary>
+        /// Iterative version of factorial.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private int iterativeFactorial(int n)
+        {
+            // If we are 1 simply return 1.
+            if (n <= 1)
+                return 1;
+            // Otherwise, instantiate result set to 1.
+            int result = 1;
+            // Loop until we get to n multiplying result by previous result.
+            for (int i = 2; i <= n; i++)
+                result *= i;
+            return result;
+        }
+
+        /// <summary>
+        /// Parse text from a text box.
+        /// </summary>
+        /// <param name="textBox"></param>
+        /// <returns></returns>
+        private int parseText(TextBox textBox)
+        {
+            try
+            {
+               return Int32.Parse(textBox.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Factorial only works on numbers!");
+                return 0;
+            }
+        }
+
+        private void btnIterative_Click(object sender, EventArgs e)
+        {
+            int n = Int32.Parse(textBox1.Text);
+            label1.Text = iterativeFactorial(n).ToString();
+        }
+
+        private void btnRecursive_Click(object sender, EventArgs e)
+        {
+            int n = Int32.Parse(textBox1.Text);
+            label1.Text = recursiveFactorial(n).ToString();
+        }
+    }
+}
